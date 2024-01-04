4715bf05ecaedb07daae0832fc6469bd762468d2
changhui98 <net_0102@naver.com>
2024-01-04 15:24:48 +0900

    게시판 틀 작업

---
+++ src/main/java/org/choongang/admin/config/repositorys/ConfigsRepository.java
@@ -0,0 +1,11 @@
+package org.choongang.admin.config.repositorys;
+
+import org.choongang.admin.config.entitys.Configs;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConfigsRepository extends JpaRepository<Configs, String> {
+
+
+
+
+}
